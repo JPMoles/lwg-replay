35a0b47d859f509873d4fc2a418bae92a9553632
James Moles <jamespmoles@gmail.com>
2023-07-06 13:56:27 -0700

    add units killed, workers supply, and army supply plots

--- graph-script.js
+++ graph-script.js
@@ -15,6 +15,18 @@ let plot2 = Plot.rectY({ length: 10000 }, Plot.binX({ y: "count" }, { x: Math.ra
 const totalSupplyDiv = document.querySelector("#totalSupplyPlot");
 totalSupplyDiv.append(plot2);
 
+let plot3 = Plot.rectY({ length: 10000 }, Plot.binX({ y: "count" }, { x: Math.random })).plot();
+const unitsLostDiv = document.querySelector("#unitsLostPlot");
+unitsLostDiv.append(plot3);
+
+let plot4 = Plot.rectY({ length: 10000 }, Plot.binX({ y: "count" }, { x: Math.random })).plot();
+const workerSupplyDiv = document.querySelector("#workerSupplyPlot");
+workerSupplyDiv.append(plot4);
+
+let plot5 = Plot.rectY({ length: 10000 }, Plot.binX({ y: "count" }, { x: Math.random })).plot();
+const armySupplyDiv = document.querySelector("#armySupplyPlot");
+armySupplyDiv.append(plot5);
+
 const replayOutputDataDiv = document.getElementById("replayOutputData");
 
 const replayOutputInterval = setInterval(() => {
@@ -43,6 +55,7 @@ const replayOutputInterval = setInterval(() => {
       playerTwoData.push(player2);
     }
 
+    // Unused Gold Plot
     plot = Plot.plot({
       grid: true,
       color: { legend: true },
@@ -71,5 +84,50 @@ const replayOutputInterval = setInterval(() => {
       ],
     });
     totalSupplyDiv.replaceChildren(totalSupplyPlot);
+
+    // Units Killed Plot
+    const unitsLostPlot = Plot.plot({
+      grid: true,
+      color: { legend: true },
+      y: {
+        label: "Units Lost",
+      },
+      marks: [
+        Plot.ruleY([0]),
+        Plot.line(playerOneData, { x: "tick", y: "unitsLost", stroke: "name" }),
+        Plot.line(playerTwoData, { x: "tick", y: "unitsLost", stroke: "name" }),
+      ],
+    });
+    unitsLostDiv.replaceChildren(unitsLostPlot);
+
+    // Workers Supply Plot
+    const workerSupplyPlot = Plot.plot({
+      grid: true,
+      color: { legend: true },
+      y: {
+        label: "Worker Supply",
+      },
+      marks: [
+        Plot.ruleY([0]),
+        Plot.line(playerOneData, { x: "tick", y: "workerSupply", stroke: "name" }),
+        Plot.line(playerTwoData, { x: "tick", y: "workerSupply", stroke: "name" }),
+      ],
+    });
+    workerSupplyDiv.replaceChildren(workerSupplyPlot);
+
+    // Army Supply Plot
+    const armySupplyPlot = Plot.plot({
+      grid: true,
+      color: { legend: true },
+      y: {
+        label: "Army Units",
+      },
+      marks: [
+        Plot.ruleY([0]),
+        Plot.line(playerOneData, { x: "tick", y: "armySupply", stroke: "name" }),
+        Plot.line(playerTwoData, { x: "tick", y: "armySupply", stroke: "name" }),
+      ],
+    });
+    armySupplyDiv.replaceChildren(armySupplyPlot);
   }
 }, 5_000);
